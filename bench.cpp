57a62da5ccc603145c4a3db553cf2220757c9e34
qyliu-hkust <qliuau@connect.ust.hk>
2022-02-24 21:48:19 +0800

    add arg parser

--- bench.cpp
+++ bench.cpp
@@ -1,18 +1,181 @@
-
+#include <boost/program_options.hpp>
 #include "utils/datautils.hpp"
 
 
 
+namespace po = boost::program_options;
+
+// benchmark R tree
+void bench_rtree() {
+
+}
+
+// benchmark quad tree
+void bench_quadtree() {
+
+}
+
+// benchmark kd tree
+void bench_kdtree() {
+
+}
+
+// benchmark uniform grid
+void bench_ug() {
+
+}
+
+// benchmark equal-depth grid
+void bench_edg() {
+
+}
+
+// benchmark ZM index
+void bench_zmindex() {
+
+}
+
+// benchmark ML index
+void bench_mlindex() {
+
+}
+
+// benchmark LISA
+void bench_lisa() {
+
+}
+
+// benchmark IF index
+void bench_ifindex() {
+
+}
+
+// benchmark RSMI
+void bench_rsmi() {
+
+}
+
+// benchmark Flood
+void bench_flood() {
+
+}
+
+// benchmark Tsunami
+void bench_tsunami() {
+
+}
+
+
 int main(int argc, char const *argv[]) {
     
-    // gen_uniform(argv[1], 2*1000*1000, 2, 1.0);
-    vec_of_double_vec_t data;
-    read_data(data, "data", 2*1000*1000, 2);
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("task", po::value<std::string>(), "tasks: gen_data, bench_index")
+        ("fname,f", po::value<std::string>(), "file name of generated data")
+        ("dist", po::value<std::string>(), "distribution used to generate data")
+        ("num,n", po::value<int>(), "number of points to be generated")
+        ("dim,d", po::value<int>(), "dimension of points to be generated")
+        ("scale,s", po::value<double>(), "distribution scale factor")
+        ("index,i", po::value<std::string>(), "index name to be benchmarked")
+    ;
 
-    for (int i=0; i<10; ++i) {
-        for (double& x : data[i]) {
-            std::cout << x << std::endl;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+
+    if (vm.count("task")) {
+        std::string task = vm["task"].as<std::string>();
+
+        if (task.compare("gen_data") == 0) {
+            if (vm.count("fname") && vm.count("dist") && vm.count("num") && vm.count("dim") && vm.count("scale")) {
+                std::string fname = vm["fname"].as<std::string>();
+                std::string dist = vm["dist"].as<std::string>();
+                int n = vm["num"].as<int>();
+                int d = vm["dim"].as<int>();
+                double s = vm["scale"].as<double>();
+
+                if (dist.compare("uniform") == 0) {
+                    // generate uniform data
+                    std::cout << "Generate " << n << " * " << d << "D Uniform" << "(0, " << s << ") data to file: " << fname << std::endl;
+                    gen_uniform(fname, n, d, s);
+                } else if (dist.compare("gaussian") == 0) {
+                    // generate gaussian data
+                    std::cout << "Generate " << n << " * " << d << "D Gaussian" << "(0, " << s << "^2) data to file: " << fname << std::endl;
+                    gen_gaussian(fname, n, d, s);
+                } else if (dist.compare("lognormal") == 0) {
+                    // generate lognormal data
+                    std::cout << "Generate " << n << " * " << d << "D Lognormal" << "(0, " << s << ") data to file: " << fname << std::endl;
+                    gen_lognormal(fname, n, d, s);
+                } else {
+                    std::cout << "Arg --dist is in [uniform, gaussian, lognormal]." << std::endl;
+                    return 1;
+                }
+            } else {
+                std::cout << "Please provide fname, dist, num, dim, and scale." << std::endl;
+                return 1;
+            }
+        } else if (task.compare("bench_index") == 0) {
+            if (vm.count("index")) {
+                std::string index = vm["index"].as<std::string>();
+                
+                if (index.compare("rtree") == 0) {
+                    std::cout << "Benchmark index: R-tree" << std::endl;
+                    bench_rtree();
+                } else if (index.compare("kdtree") == 0) {
+                    std::cout << "Benchmark index: kd-tree" << std::endl;
+                    bench_kdtree();
+                } else if (index.compare("quadtree") == 0) {
+                    std::cout << "Benchmark index: quad-tree" << std::endl;
+                    bench_quadtree();
+                } else if (index.compare("ug") == 0) {
+                    std::cout << "Benchmark index: uniform grid" << std::endl;
+                    bench_ug();
+                } else if (index.compare("edg") == 0) {
+                    std::cout << "Benchmark index: equal-depth grid" << std::endl;
+                    bench_edg();
+                } else if (index.compare("zm") == 0) {
+                    std::cout << "Benchmark index: ZM index" << std::endl;
+                    bench_zmindex();
+                } else if (index.compare("ml") == 0) {
+                    std::cout << "Benchmark index: ML index" << std::endl;
+                    bench_mlindex();
+                } else if (index.compare("lisa") == 0) {
+                    std::cout << "Benchmark index: LISA" << std::endl;
+                    bench_lisa();
+                } else if (index.compare("if") == 0) {
+                    std::cout << "Benchmark index: IF index" << std::endl;
+                    bench_ifindex();
+                } else if (index.compare("rsmi") == 0) {
+                    std::cout << "Benchmark index: RSMI" << std::endl;
+                    bench_rsmi();
+                } else if (index.compare("flood") == 0) {
+                    std::cout << "Benchmark index: Flood" << std::endl;
+                    bench_flood();
+                } else if (index.compare("tsunami") == 0) {
+                    std::cout << "Benchmark index: Tsunami" << std::endl;
+                    bench_tsunami();
+                } else {
+                    std::cout << "Arg --index is in [rtree, kdtree, quadtree, ug, edg, zm, ml, lisa, if, rsmi, flood, tsunami]." << std::endl;
+                    return 1;
+                }
+            } else {
+                std::cout << "Please provide index." << std::endl;
+                return 1;
+            }
+        } else {
+            std::cout << "Arg --task is in [gen_data, bench_index]." << std::endl;
+            return 1;
         }
+    } else {
+        std::cout << "Specify a task." << std::endl;
+        std::cout << desc << std::endl;
+        return 1;
     }
     
     return 0;
--- utils/datautils.hpp
+++ utils/datautils.hpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <tpie/file_stream.h>
 #include <assert.h>
 #include "type.hpp"
@@ -52,9 +53,11 @@ void gen_lognormal(const std::string& fname, const int n, const int d, const dou
     tpie::tpie_init();
     tpie::file_stream<double> out;
     out.open(fname);
+
     for (int i=0; i<n*d; ++i) {
         out.write(dis(gen));
     }
+
     out.close();
     tpie::tpie_finish();
 }
@@ -74,6 +77,7 @@ void read_data(vec_of_double_vec_t& vv, const std::string& fname, const int n, c
     tpie::tpie_init();
     tpie::file_stream<double> in;
     in.open(fname);
+
     for (int i=0; i<n; ++i) {
         vv[i].resize(d);
         for (int j=0; j<d; ++j) {
@@ -81,9 +85,43 @@ void read_data(vec_of_double_vec_t& vv, const std::string& fname, const int n, c
         }
     }
 
+    // make sure the input data size is n*d 
     assert(!in.can_read());
 
     in.close();
     tpie::tpie_finish();
 }
+
+
+// trasform csv format data to binary data
+void csv_to_bin(const std::string& in_fname, const std::string& out_fname, const char delimiter, const int n, const int d) {
+    std::ifstream in_fs(in_fname);
+    tpie::tpie_init();
+    tpie::file_stream<double> out;
+    out.open(out_fname);
+
+    std::string line;
+    int count_n = 0;
+    while (std::getline(in_fs, line)) {
+        std::stringstream line_stream(line);
+        std::string cell;
+        int count_d = 0;
+        while (std::getline(line_stream, cell, delimiter)) {
+            out.write(std::stod(cell));
+            count_d++;
+        }
+
+        // chech dimension
+        assert(count_d == d);
+        count_n++;
+    }
+
+    // check total size
+    assert(count_n == n);
+
+    in_fs.close();
+    out.close();
+    tpie::tpie_finish();
+}
+
 
